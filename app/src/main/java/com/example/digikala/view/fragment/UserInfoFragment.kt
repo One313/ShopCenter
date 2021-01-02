eedfcd80cffdc0d1682de5a04edf27b7dbb4caec
One313 <farahani.hr@gmail.com>
2021-01-02 17:00:28 +0330

    We added workmanager to fetch new product and send notification.

--- app/src/main/java/com/example/digikala/view/fragment/UserInfoFragment.kt
+++ app/src/main/java/com/example/digikala/view/fragment/UserInfoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.digikala.databinding.FragmentUserInfoBinding
 import com.example.digikala.viewmodel.CustomerViewModel
 
@@ -26,6 +27,10 @@ class UserInfoFragment : Fragment() {
         if (c != null) {
             binding.textViewUsetName.text = c.mFistName+" "+ c.mLastName
             binding.textViewUserEmail.text = c.mEmail
+        }
+        binding.imageViewNotifSetting.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                    .navigate(UserInfoFragmentDirections.actionUserInfoFragmentToNotificationSettingFragment())
         }
         return binding.root
     }
